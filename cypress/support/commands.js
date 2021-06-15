b7fe52922350420c43127f1761eac70671152bfa
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-06-15 10:29:56 -0600

    Add fixtures and cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('stubData', () => {
+  const baseUrl = 'http://localhost:3001/api/v1'
+  cy.fixture('urlData.json')
+    .then(data => {
+      cy.intercept(`${baseUrl}/urls`, {
+        statusCode: 201,
+        delay: 100,
+        body: data
+      })
+    })
+})
